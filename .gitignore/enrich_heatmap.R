404b16773dbd3cb28b94fa1ae07e3b17d7db5212
JonesMacke <34124087+JonesMacke@users.noreply.github.com>
2017-11-30 18:01:09 +0800

    enrich_heatmap.R

---
+++ .gitignore/enrich_heatmap.R
@@ -0,0 +1,18 @@
+library(cluster)
+library(gplots)
+#library(Biobase)
+args=commandArgs(T)
+data = read.table(args[1], header=T, com='', sep="\t",row.names=1,quote = "")
+data = as.matrix(data)
+data = -log10(data)
+#centered_data = t(scale(t(data), scale=T,center=T))
+#hc_genes = agnes(centered_data, diss=FALSE, metric="euclidean",method="complete")
+#myheatcol = greenred(255)
+#myheatcol=colorRampPalette(c("DodgerBlue3","yellow","red"))(255)
+myheatcol=colorRampPalette(c("#4575B4", "white","#D73027"))(50)
+nr=nrow(data);
+nc=ncol(data);
+pdf(file=args[2],width=5+nc*0.7, height=4+0.2*nr, paper="special")
+title=sub(pattern=".fpkm",replacement="",args[1])
+heatmap.2(data, dendrogram='none', Rowv=TRUE,Colv=FALSE,col=myheatcol, scale="row", symkey="T",density.info="none", trace="none", key=TRUE, keysize=1, cexCol=2, cexRow= 0.8+1/log10(nr), margins=c(2,17), main=title,lmat=rbind(c(0,3,4),c(2,1,0)),lhei=c(0.6*nr/(5+0.4*nr),0.2*nr),lwid=c(0.37*(5+nc)/2/nc/2,(5+nc)/2,0.37*(20+4*nc)/2/nc*9/8) ,srtCol=0, adjCol=c(0.5,-(2.51+0.2*nr)*55/8.79))
+dev.off()
